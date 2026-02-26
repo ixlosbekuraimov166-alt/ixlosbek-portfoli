e8de94c6b3883b9b2b75cf9ef639df9fdce6768b
ixlosbekuraimov166-alt <ixlosbekuraimov166@gmail.com>
2026-02-26 17:12:04 +0500

    Mening ikkinchi veb sayttim eskisini to`g`irladim
    Mening ikkinchi veb saytim eskisini to`g`irladim

---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("btn");
+let text = document.getElementById("text");
+
+button.addEventListener("click", function() {
+    text.textContent = "Men endi JavaScript ham o'rganayapman 🚀";
+});
